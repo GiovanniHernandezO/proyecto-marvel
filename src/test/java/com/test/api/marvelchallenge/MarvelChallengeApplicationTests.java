51b61793d6790fcb634e573fd6bdee957c50e85f
GiovanniHernandezO <giovanni.hernandez.olivares@gmail.com>
2023-10-22 19:48:57 -0300

    commit inicial

---
+++ src/test/java/com/test/api/marvelchallenge/MarvelChallengeApplicationTests.java
@@ -0,0 +1,8 @@
+package com.test.api.marvelchallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarvelChallengeApplicationTests {
+}
